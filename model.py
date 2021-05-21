d6097a5c666f5469ae4e1da84a744f34b86382a8
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-05-21 16:16:50 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pickle
-dataframe = pd.read_csv(r'C:\Users\PRAMILA\Downloads\Placementsample1.csv')
+dataframe = pd.read_csv('Placementsample1.csv')
 def convert_to_int(word):
     word_dict = {'Good':1, 'Bad':0,'yes':1,'no':0,'completed':1,'none':0}
     return word_dict[word]
@@ -11,7 +11,7 @@ regressor = RandomForestClassifier(n_estimators=10,criterion='entropy')
 #Fitting model with training data
 regressor=regressor.fit(X, y)
 # Saving model to disk
-pickle.dump(regressor, open('model.pkl','wb'))
+pickle.dump(regressor, open('final_model.pkl','wb'))
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
 print(model.predict([[convert_to_int('Good'),69.6,68.4,78.3,5,convert_to_int('completed'),60,convert_to_int('yes'),80]]))
